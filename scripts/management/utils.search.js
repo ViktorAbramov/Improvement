41fe560aced7c671b30ae5eb409a5c716fe1e248
viktor.abramov <viktor.abramov@sethq.com>
2014-04-04 15:29:09 +0300

    Added annotation

--- scripts/management/utils.search.js
+++ scripts/management/utils.search.js
@@ -3,21 +3,35 @@
 /** @constructor */
 utils.Gallery = function() {
 
+  /** @type {string}
+   * @conts
+   */
   var API_URL = 'http://api.flickr.com/services/feeds/photos_public.gne';
 
+  /** @type {string}
+   * @conts
+   */
   var CONTAINER_ID = 'image-container';
 
+  /**
+   * Initializes module gallery.
+   * @private
+   */
   function init_() {
     var form = document.forms[0];
     form.onsubmit = onsubmit_;
   }
 
+  /**
+   * Performs onsubmit action.
+   * @return {boolean}
+   * @private
+   */
   function onsubmit_() {
     var jsonp = new utils.Jsonp;
-    /** @type {Element} */
       var searchArea = this.getElementsByTagName('INPUT')[0];
     var searchKey = searchArea.value;
-    /** @type {!Array.<string>} */var tags = [];
+    var tags = [];
     if (searchKey && searchKey.length) {
       tags = toCorrectTag_(searchKey);
       jsonp.req(API_URL, {tags: tags, format: 'json'}, function(response) {
@@ -27,14 +41,25 @@ utils.Gallery = function() {
     return false;
   }
 
+  /**
+   * Transform searchin keywords into keywords atrray.
+   * @param {string} keywords Searching keywords.
+   * @return {!Array}
+   * @private
+   */
   function toCorrectTag_(keywords) {
     return keywords.split(' ').join(',');
   }
 
+  /**
+   * Draws images from server's response.
+   * @param {?Object} opt_images Object with images.
+   * @private
+   */
   function draw_(opt_images) {
     var container = document.getElementById(CONTAINER_ID);
     container.innerHTML = '';
-    if (opt_images) {
+    if (opt_images && opt_images.length) {
       var length = opt_images.length;
       for (var i = 0; i < length; i++) {
         var image = opt_images[i];
@@ -43,12 +68,23 @@ utils.Gallery = function() {
     }
   }
 
+  /**
+   * Draws single image.
+   * @param {!Object} image Image's object.
+   * @return {Element}
+   * @private
+   */
   function drawImage_(image) {
-    /** @type {Element} */ var picture = document.createElement('IMG');
+    var picture = document.createElement('IMG');
     picture.src = image['media']['m'];
     return picture;
   }
+
   init_();
 };
 
+/**
+ * Register single module instance.
+ * @type {!utils.Gallery}
+ */
 utils.gallery = new utils.Gallery;
