a4414903ee8f86c22ec2b05c3e5510e0df2f475d
Viktor Abramov <viktor.abramov@sethq.com>
2014-04-15 22:30:46 +0300

    Update utils.storage.js

--- scripts/utils/utils.storage.js
+++ scripts/utils/utils.storage.js
@@ -1,21 +1,21 @@
-
 utils.Storage = function() {
 
-	var storage_ = window['localStorage'];
+  var storage_ = window['localStorage'];
 
-	this.get = function(key) {
-		return storage_.getItem(key);
-	}
+  this.get = function(key) {
+    return storage_.getItem(key);
+  }
 	
-	this.set = function(key, value) {
-		storage_.setItem(key, value);
-	}
+  this.set = function(key, value) {
+    storage_.setItem(key, value);
+  }
 	
-	this.drop = function(key) {
-		storage_.removeItem(key);
-	}
+  this.drop = function(key) {
+    storage_.removeItem(key);
+    }
 
-	this.clear = function() {
-		storage_.clear();
-	}
+  this.clear = function() {
+    storage_.clear();
+  }
+  
 }
